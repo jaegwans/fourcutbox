e96bf5170dda08856a5fd2308df225d70c2aae6c
jaegwans <skhu201811140@gmail.com>
2025-01-28 22:03:45 +0900

    fix: remove react-native-splash-screen

--- App.tsx
+++ App.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import SplashScreen from 'react-native-splash-screen';
 import {RootNavigator} from './src/navigation/navigation';
 
 const App = () => {
-  React.useEffect(() => {
-    SplashScreen.hide();
-  }, []);
-
   return (
     <SafeAreaProvider>
       <NavigationContainer>
