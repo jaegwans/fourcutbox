6288caa0283f3b4856cc54f53cf8ebb4bbcc6ad4
jgkimdeepbio <jgkim@deepbio.co.kr>
2025-02-01 18:46:23 +0900

    feat: splash image

--- App.tsx
+++ App.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import React, {useEffect} from 'react';
+import RNBootSplash from 'react-native-bootsplash';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {RootNavigator} from './src/navigation/navigation';
 
-const App = () => {
+function App(): JSX.Element {
+  useEffect(() => {
+    // 앱 초기화가 완료되면 스플래시 스크린을 숨깁니다
+    const init = async () => {
+      // 여기에 필요한 초기화 작업을 추가할 수 있습니다
+      await RNBootSplash.hide({fade: true});
+    };
+
+    init();
+  }, []);
+
   return (
     <SafeAreaProvider>
       <NavigationContainer>
@@ -11,6 +22,6 @@ const App = () => {
       </NavigationContainer>
     </SafeAreaProvider>
   );
-};
+}
 
 export default App;
