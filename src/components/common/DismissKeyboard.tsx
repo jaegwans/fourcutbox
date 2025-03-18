e0248fe5139563e7c4db0213e251b1e36b2ccb45
jgkimdeepbio <jgkim@deepbio.co.kr>
2025-03-18 19:13:20 +0900

    feat: DismissKeyboard components

---
+++ src/components/common/DismissKeyboard.tsx
@@ -0,0 +1,18 @@
+import React, {ReactNode} from 'react';
+import {Keyboard, TouchableWithoutFeedback, View} from 'react-native';
+
+interface DismissKeyboardProps {
+  children: ReactNode;
+}
+
+const DismissKeyboard = ({children}: DismissKeyboardProps) => {
+  return (
+    <TouchableWithoutFeedback
+      onPress={() => Keyboard.dismiss()}
+      accessible={false}>
+      <View style={{flex: 1}}>{children}</View>
+    </TouchableWithoutFeedback>
+  );
+};
+
+export default DismissKeyboard;
