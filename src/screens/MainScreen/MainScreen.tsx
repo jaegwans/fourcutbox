1f532440795bdaa53b51d7cba3743d50d48602c8
jgkimdeepbio <jgkim@deepbio.co.kr>
2025-03-18 20:40:05 +0900

    fix: main safeview

--- src/screens/MainScreen/MainScreen.tsx
+++ src/screens/MainScreen/MainScreen.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Text} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import {styles} from './MainScreen.styles';
 
 export const MainScreen = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <Text>메인 화면 (저장된 사진 목록)</Text>
     </SafeAreaView>
   );
